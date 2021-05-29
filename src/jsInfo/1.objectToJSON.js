eef65ec13365b1d996a95d09edd2db1a08b3dd4a
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-29 10:11:49 +0400

    Create(1. Object to JSON): Created file and resolved task

---
+++ src/jsInfo/1.objectToJSON.js
@@ -0,0 +1,11 @@
+// Turn the object into JSON and back
+let user = {
+  name: "John Smith",
+  age: 35
+}
+
+const toJSON = JSON.stringify(user)
+const JsonParse = JSON.parse(toJSON)
+
+console.log(toJSON) // {"name":"John Smith","age":35}
+console.log(JsonParse) // { name: 'John Smith', age: 35 }
